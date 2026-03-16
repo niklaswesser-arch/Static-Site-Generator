9d2b99388f135980c672b012f8afc477d5a13fc2
niklaswesser-arch <niklas.wesser@gmail.com>
2026-03-16 21:00:32 +0100

    latest changes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,38 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_static_to_public(src="static", dest="public"):
+    if os.path.exists(dest):
+        print(f"Deleting {dest} directoy...")
+        shutil.rmtree(dest)
+    
+    print(f"Creating {dest} directory...")
+    os.mkdir(dest)
+
+    copy_directory_contents(src,dest)
+
+def copy_directory_contents(src, dest):
+    items = os.listdir(src)
+    
+    for item in items:
+        src_path = os.path.join(src, item)
+        dest_path = os.path.join(dest, item)
+
+        if os.path.isfile(src_path):
+            print(f"Copying file: {src_path} -> {dest_path}")
+            shutil.copy(src_path, dest_path)
+        else:
+            print(f"Creating directory: {dest_path}")
+            os.mkdir(dest_path)
+            copy_directory_contents(src_path, dest_path)
+
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    copy_static_to_public()
+    print("\nStatic files copied successfully!")
 
-main()
+
+
+if __name__ == "__main__":
+     main()
+    

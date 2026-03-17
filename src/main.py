a5ea6528673cddf37455f41be47b365fc1f7ff30
niklaswesser-arch <niklas.wesser@gmail.com>
2026-03-17 09:10:46 +0100

    latest changes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import os
 import shutil
 from markdown import markdown_to_html_node, extract_title
+import sys
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     print(f"Generating path from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, 'r') as f:
@@ -16,8 +17,8 @@ def generate_page(from_path, template_path, dest_path):
 
     title = extract_title(markdown_content)
 
-    final_html = template_content.replace("{{ Title }}", title)
-    final_html = final_html.replace("{{ Content }}", html_content)
+    final_html = template_content.replace('href="/', f'href="{basepath}')
+    final_html = final_html.replace('src="/', f'src="{basepath}')
 
     dest_dir = os.path.dirname(dest_path)
     if dest_dir and not os.path.exists(dest_dir):
@@ -29,7 +30,7 @@ def generate_page(from_path, template_path, dest_path):
     print(f"Page generated at {dest_path}")
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath="/"):
     if not os.path.exists(dir_path_content):
         raise ValueError(f"Content directory does not exist: {dir_path_content}")
     
@@ -43,19 +44,18 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                 html_filename = item.replace('.md', '.html')
                 dest_path = os.path.join(dest_dir_path, html_filename)
                 
-                generate_page(from_path, template_path, dest_path)
+                generate_page(from_path, template_path, dest_path, basepath)
         else:
             new_dest_dir = os.path.join(dest_dir_path, item)
             
-            # Skapa katalogen om den inte finns
             if not os.path.exists(new_dest_dir):
                 os.makedirs(new_dest_dir)
                 print(f"Creating directory: {new_dest_dir}")
             
-            generate_pages_recursive(from_path, template_path, new_dest_dir)
+            generate_pages_recursive(from_path, template_path, new_dest_dir, basepath)
 
 
-def copy_static_to_public(src="static", dest="public"):
+def copy_static_to_public(src="static", dest="docs"):
     if os.path.exists(dest):
         print(f"Deleting {dest} directoy...")
         shutil.rmtree(dest)
@@ -82,15 +82,22 @@ def copy_directory_contents(src, dest):
 
 
 def main():
-    copy_static_to_public()
-
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    
+    print(f"Using basepath: {basepath}")
+    
+    copy_static_to_public(src="static", dest="docs")
+    
     generate_pages_recursive(
         dir_path_content="content",
         template_path="template.html",
-        dest_dir_path="public"
+        dest_dir_path="docs", 
+        basepath=basepath
     )
-
-    print("\nWebsite generated!")
+    
+    print("\nWebsite generated successfully!")
 
 
 if __name__ == "__main__":
